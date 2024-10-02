f5b28ed9a1268df4d0ae1543c9508282c8fd9fcc
MaheshPaade <116006160+mahesh2i3u893y1y@users.noreply.github.com>
2024-10-02 07:38:44 +0530

    constans.jsx

--- src/utilities/constans.jsx
+++ src/utilities/constans.jsx
@@ -3,7 +3,7 @@ export const OPTIONS    = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer'+ process.env.REACT_APP_TMDB_KEY,
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMDRhNjAyNGEyNTJkYTJmMWY0MzJjMTk0MThhYWZmMCIsIm5iZiI6MTcyNzMzNDI4NS4yNjY4NDMsInN1YiI6IjY2ZjUwNWMzMzg5NDJhYjg5MGVkM2MyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jy4VUilV9u5zR7TJZs2S9jB3tnO2tFmQcENwsY7w9O8',
     }
   };
   
